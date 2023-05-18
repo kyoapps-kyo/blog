815b1b659369cca6769c16b7aeb22439bb83e4f6
kyoapps-kyo <kyo@kyoapps.com>
2023-05-18 19:44:14 +0800

    update: posts page

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -3,12 +3,21 @@
         <h2 class="text-4xl font-bold mb-10 text-base-content">Posts</h2>
         <template v-for="key in Object.keys(navItemsByYear)">
             <div class=" relative min-h-[8rem] mb-10">
-                <h2 class="absolute -top-10 -left-10 text-[9rem] leading-[9rem] opacity-20 stroke-text font-mono z-0 pointer-events-none">{{ key }}</h2>
+                <h2
+                    class="absolute -top-10 -left-10 text-[9rem] leading-[9rem] opacity-20 stroke-text font-mono z-0 pointer-events-none">
+                    {{ key }}</h2>
                 <div class=" min-h-[6rem] text-xl text-base-content font-medium text-opacity-90 px-6 z-10">
                     <ContentNavigation :navigation="(navItemsByYear[key] as NavItem[])"></ContentNavigation>
                 </div>
             </div>
         </template>
+        <p class="font-mono">
+            <span>&gt;&nbsp;&nbsp;</span>
+            <NuxtLink @click.stop="router.go(-1)">
+                <span class="underline underline-offset-8 decoration-1">cd&nbsp;..</span>
+            </NuxtLink>
+            <span class="cursor text-lg"></span>
+        </p>
     </div>
 </template>
 
@@ -21,10 +30,11 @@ interface NavItem {
     navigation: navigation
 }
 interface navigation {
-  title: string
-  date: string
-  [key: string]: any
+    title: string
+    date: string
+    [key: string]: any
 }
+const router = useRouter()
 const queryNavItems = async (): Promise<NavItem[]> => {
     const navItems = await queryContent('posts', 'articles').find()
     return navItems
@@ -47,4 +57,21 @@ const navItemsByYear = await classifyNavItemsByYear()
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.cursor {
+    display: inline-block;
+    width: 2px;
+    height: 16px;
+    background-color: hsl(var(--bc));
+    animation: blink 1s linear infinite;
+    position: relative;
+    top: 2px;
+    left: 8px;
+}
+
+@keyframes blink {
+    50% {
+        opacity: 0;
+    }
+}
+</style>
